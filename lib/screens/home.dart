d082eefb82c30693987414dfbff5f8f84c5e080a
Onifade Joshua <onifadjosh@gmail.com>
2024-05-08 08:31:06 +0100

    Updated API call to fetch 100 users, refactored ListView builder for user data display

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,9 +6,8 @@ import 'dart:convert';
 
 
 
-
 class Home extends StatefulWidget {
-  
+  // final int length;
 
   const Home({super.key});
 
@@ -28,7 +27,7 @@ class _HomeState extends State<Home> {
   
     print('na man you be');
     final response = await http.get(
-      Uri.parse('https://randomuser.me/api/?results=5'),
+      Uri.parse('https://randomuser.me/api/?results=100'),
       // headers: {
       //   'Authorization': 'Bearer $authToken',
       //   'Content-Type': 'application/json',
@@ -40,7 +39,7 @@ class _HomeState extends State<Home> {
 
     if (response.statusCode==200){
       // print('LFG');
-      print (response.body);
+      // print (response.body);
       return jsonDecode(response.body);
       // print (jsonDecode(response.body));
 
@@ -54,7 +53,7 @@ class _HomeState extends State<Home> {
   
   }
 
-  List<dynamic> users = [];
+  // List<dynamic> users = [];
 
   
   @override
@@ -68,23 +67,53 @@ class _HomeState extends State<Home> {
         
           if (snapshot.connectionState == ConnectionState.waiting) {
           // If the future is still in progress, display a loading indicator
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
           }else if (snapshot.hasError) {
           // If an error occurs, display the error message
           return Center(child: Text('Error: ${snapshot.error}'));
           } else {
           // If the future completes successfully, print the data
-          print(snapshot.data);
+          // print(snapshot.data);
           // Here, you can return a widget to display the fetched data
-          final user = users[index];
-          final email = user['email'];
-          return ListTile(); // Placeholder widget
+            final List<dynamic> users = snapshot.data!['results'];
+          // final email = users['email'];
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              return Container(
+                margin: EdgeInsets.only(bottom: 10, right: 10, left: 10, top: 10),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [BoxShadow(
+                     color: Colors.grey,
+                     offset: Offset(2, 2), 
+                  )]
+
+                ),
+                child: ListTile(
+                  title: Text('${user['name']['first']} ${user['name']['last']}'),
+                  subtitle: Text('${user['location']['city']}, ${user['email']}'),
+                  leading: Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.network('${user['picture']['thumbnail']}'),
+                    ),
+                  ),
+                ),
+    
+              );
+              
+            },
+          ); // Placeholder widget
         }}
     ));
   }
 
   AppBar _appBar() {
     return AppBar(
+      
       backgroundColor: Colors.blue,
       title:const Row(
         // mainAxisAlignment: MainAxisAlignment.spaceBetween,
